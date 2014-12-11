bb475df9b1d4489bad6e4e3e6a8697c3d84e0848
Hebi Li <lihebi.ga@gmail.com>
2014-12-11 12:05:31 -0600

    remove gitbook support

--- README.md
+++
@@ -1,3 +0,0 @@
-# Wiki
-
-Both webpage and gitbook.
--- gen-roadmap.py
+++ gen-roadmap.py
@@ -20,21 +20,8 @@ for root,dirs,files in os.walk('src'):
   files.sort()
   for f in files:
     add(root, f)
-
-js = json.dumps(result,indent=2)
+od = collections.OrderedDict(sorted(result.items()))
+js = json.dumps(od,indent=2)
 with open('roadmap.json', 'w') as f:
   f.write(js)
 print('generated roadmap.json')
-
-def func(node, path, indent, f):
-    for k in node.keys():
-        if (node[k]==False):
-            f.write(' '*indent+'* ['+k+']('+path+'/'+k+')\n')
-        else:
-            f.write(' '*indent+'* ['+k+']()\n')
-            func(node[k], path+'/'+k, indent+2, f)
-
-
-with open('SUMMARY.md', 'w') as f:
-    func(result, 'src', 0, f)
-print('generated SUMMARY.md')
